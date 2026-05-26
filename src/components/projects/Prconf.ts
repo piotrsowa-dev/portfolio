e15df36c5b53bee84de687bfd42a55d0b039dcc0
Piotr Sowa <piotrek.sowa.dev@gmail.com>
2026-05-26 17:32:25 +0200

    fix image paths

--- src/components/projects/Prconf.ts
+++ src/components/projects/Prconf.ts
@@ -1,7 +1,7 @@
 export const prconfs = [
     {
         id: 1,
-        image: "/assetsphotos/fryzjer.png",
+        image: `${import.meta.env.BASE_URL}assetsphotos/fryzjer.png`,
         name: "Fryzjer | Demo",
         description: "Strona one page, landing page dla fryzjer w katowicach zrobiony przy użyciu technologi html, css. Do wersji demo użyto zdjęc wygenerowanych przez AI.",
         data: "2026",
@@ -10,7 +10,7 @@ export const prconfs = [
     },
     {
         id: 2,
-        image: "/assetsphotos/bialaperla.png",
+        image: `${import.meta.env.BASE_URL}assetsphotos/bialaperla.png`,
         name: "Biała Perła",
         description: 'Landing page dla sali okolicznościowej „Biała Perła”, wykonany na zlecenie klienta. Strona została zbudowana w HTML, CSS i JavaScript, z animowanym tłem, galerią zdjęć oraz eleganckim, nowoczesnym wyglądem dopasowanym do branży eventowej.',
         data: "2023",
@@ -19,7 +19,7 @@ export const prconfs = [
     },
     {
         id: 3,
-        image: "/assetsphotos/fivemhud.png",
+        image: `${import.meta.env.BASE_URL}assetsphotos/fivemhud.png`,
         name: "Customizable Game UI",
         description: 'Konfigurowalny system HUD zaprojektowany dla serwerów FiveM, umożliwiający dynamiczną zmianę koloru oraz skali interfejsu użytkownika. Projekt został wykonany z użyciem HTML, CSS i JavaScript, skupiając się na estetyce, czytelności i personalizacji UI.',
         data: "2024",
@@ -28,7 +28,7 @@ export const prconfs = [
     },
     {
         id: 4,
-        image: "/assetsphotos/portfolio.png",
+        image: `${import.meta.env.BASE_URL}assetsphotos/portfolio.png`,
         name: "Personal Portfolio",
         description: 'Osobiste portfolio frontend developera stworzone w React i TypeScript. Strona prezentuje moje projekty, stack technologiczny oraz doświadczenie, skupiając się na responsywnym layoucie, komponentowej strukturze i minimalistycznym UI.',
         data: "2026",
