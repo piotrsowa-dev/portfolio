fbfcc3f9f66b434825e62945fa1ae1586a4cf9a7
Piotr Sowa <piotrek.sowa.dev@gmail.com>
2026-05-27 17:32:00 +0200

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import Hero from "./components/hero/Hero"
 import About from "./components/about/About"
 import Projects from "./components/projects/Projects"
 import Contact from "./components/contact/Contact"
+import Footer from "./components/footer/Footer"
 import "./Main.css"
 
 function App() {
 
   return (
     <>
-        <Nav></Nav>
-        
+      <Nav></Nav>
       <div className="container">
         <Hero></Hero>
         <About></About>
         <Projects></Projects>
         <Contact></Contact>
       </div>
+      <Footer></Footer>
+
     </>
   )
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import "./Footer.css"
+function Footer() {
+  return (
+    <footer>
+        <p>&copy; 2026 Piotr Sowa. Built with React</p>
+        <div className="adv-fot">
+            <a href="https://github.com/piotrsowa-dev">GitHub</a>
+            <a href="mailto:piotrek.sowa.dev@gmail.com">Email</a>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
